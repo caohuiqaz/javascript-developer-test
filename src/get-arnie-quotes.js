783aa8b6514f02fa463d06a89500f15fe3c272d0
Hui Cao <caohuiqaz@gmail.com>
2021-06-27 20:11:21 +1000

    complete

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -3,6 +3,29 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
   // return results;
+
+  const results = Promise.all(
+		urls.map(async (url) => {
+			try {
+        // Get request from url, generate { [key] : message } object
+        const res = await httpGet(url);
+        var key = "";
+        const message = JSON.parse(res.body).message;
+        
+        if (res.status == 200) {
+          key = "Arnie Quote";
+        } else {
+          key = "FAILURE";
+        }
+        return { [key]: message };
+
+			} catch (err) {
+				console.log("Error(getArnieQuotes): ", err);
+			}
+		})
+	);
+
+  return results;
 };
 
 module.exports = {
